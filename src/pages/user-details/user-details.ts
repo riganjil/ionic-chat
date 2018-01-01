0ac8dcb4dc8873895b7510cb3af4194503347646
arimaulana <ari.maulana08@gmail.com>
2018-01-02 05:13:28 +0700

    Search user

--- src/pages/user-details/user-details.ts
+++ src/pages/user-details/user-details.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { GithubUsers } from "../../providers/github-users/github-users";
+import {User} from "../../models/user";
+
 /**
  * Generated class for the UserDetailsPage page.
  *
@@ -14,10 +17,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'user-details.html',
 })
 export class UserDetailsPage {
+  user: User;
   login: string;
 
-  constructor(public navCtrl: NavController, navParams: NavParams) {
+  constructor(public navCtrl: NavController, navParams: NavParams, private githubUsers: GithubUsers) {
     this.login = navParams.get('login');
+    githubUsers.loadDetails(this.login).subscribe(user => {
+      this.user = user;
+      console.log(user)
+    })
   }
 
   ionViewDidLoad() {
